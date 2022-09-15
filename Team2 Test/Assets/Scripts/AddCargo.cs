c7e175849396794abc20bb50ef44fb7d31882d71
LexaSt <astajarin@mail.ru>
2022-09-15 23:38:02 +0300

    AddCargo

---
+++ Team2 Test/Assets/Scripts/AddCargo.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddCargo : MonoBehaviour
+{
+    public GameObject CubePref;
+    public GameObject SpherePref;
+    public GameObject CapsulePref;
+    public GameObject CilinderPref;
+
+    public void CreateCargo()
+    {
+        Vector3 Rand = new Vector3(0, 0, Random.Range(-7, -12));
+        if (Random.Range(1, 4) == 1)
+        {
+            Instantiate(CubePref, transform.position+ Rand, transform.rotation);
+        }
+        else if (Random.Range(1, 4) == 2)
+        {
+            Instantiate(SpherePref, transform.position + Rand, transform.rotation);
+        }
+        else if (Random.Range(1, 4) == 3)
+        {
+            Instantiate(CapsulePref, transform.position + Rand, transform.rotation);
+        }
+        else if (Random.Range(1, 4) == 4)
+        {
+            Instantiate(CilinderPref, transform.position + Rand, transform.rotation);
+        }
+
+
+    }
+}
