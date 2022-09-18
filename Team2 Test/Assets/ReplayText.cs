99079f2885e48b50e19d74bdd8b7a7210183fe96
Long9696 <longark96@gmail.com>
2022-09-18 21:13:02 +0700

    Всплывающие подсказки при наведении мышки

--- Team2 Test/Assets/ReplayText.cs
+++ Team2 Test/Assets/ReplayText.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ReplayText : MonoBehaviour
+public class ReplayText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] Text _replayText;
+    [SerializeField] GameObject _replayText;
 
-    private void OnMouseEnter()
+    
+
+    public void OnPointerExit(PointerEventData eventData)
     {
-        _replayText.enabled = true;
+        _replayText.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _replayText.SetActive(true);
     }
 }
