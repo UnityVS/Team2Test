d6eeb61fb463301ae09e721f6b52f2fd76d3b023
UnityVS <101996841+UnityVS@users.noreply.github.com>
2022-09-18 18:44:10 +0300

    add cargo +1 bug fix cargo script

--- Team2 Test/Assets/Scripts/AddCargo.cs
+++ Team2 Test/Assets/Scripts/AddCargo.cs
@@ -10,19 +10,19 @@ public class AddCargo : MonoBehaviour
     public void CreateCargo()
     {
         Vector3 Rand = new Vector3(0, 0, Random.Range(-5, -10));
-        if (Random.Range(1, 4) == 1)
+        if (Random.Range(1, 5) == 1)
         {
             Instantiate(CubePref, transform.position+ Rand, Quaternion.identity);
         }
-        else if (Random.Range(1, 4) == 2)
+        else if (Random.Range(1, 5) == 2)
         {
             Instantiate(SpherePref, transform.position + Rand, transform.rotation);
         }
-        else if (Random.Range(1, 4) == 3)
+        else if (Random.Range(1, 5) == 3)
         {
             Instantiate(CapsulePref, transform.position + Rand, transform.rotation);
         }
-        else if (Random.Range(1, 4) == 4)
+        else if (Random.Range(1, 5) == 4)
         {
             Instantiate(CilinderPref, transform.position + Rand, transform.rotation);
         }
